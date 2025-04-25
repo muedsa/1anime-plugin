8da240dc2b76f645e82ba24f25fde41314a70976
MUEDSA <7676275+muedsa@users.noreply.github.com>
2025-04-25 10:33:58 +0800

    release: 0.0.13

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -25,8 +25,8 @@ android {
         applicationId = "com.muedsa.tvbox.an1me"
         minSdk = 24
         targetSdk = 35
-        versionCode = 12
-        versionName = "0.0.12"
+        versionCode = 13
+        versionName = "0.0.13"
     }
 
     signingConfigs {
@@ -87,7 +87,7 @@ tasks.withType<DexMergingTask> {
     doLast {
         println("taskName: $name $variantName")
         if (name.startsWith("mergeExtDex")) {
-            outputDir.get().asFile.listFiles().forEach {
+            outputDir.get().asFile.listFiles()?.forEach {
                 removePackagesFromDex(
                     file = it,
                     excludedPackages = listOf("kotlin", "kotlinx", "org.intellij", "org.jetbrains"),
